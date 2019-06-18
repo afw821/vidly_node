d812f43b029235598c538755facbdbcd6db6bf98
Alexander Watkins <afw821@gmail.com>
2019-06-18 00:06:06 -0400

    I began to write out the genre model and implement the Joi validate function

---
+++ models/genre.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+//define your GENRE Schema
+const genreSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 50
+    }
+});
+
+const Genre = mongoose.model('Genre', genreSchema);
+
+//Joi validation function to export and validate USER REQ OBJECT
+function validateGenre(genre) {
+    //define a schema to validate
+    const schema = {
+        name: Joi.string().min(3).max(50).required()
+    };
+    return Joi.validate(genre, schema);
+}
