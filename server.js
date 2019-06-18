7e45aa9cb77516d14b8d0bce2ca7cc9f15cfec94
Alexander Watkins <afw821@gmail.com>
2019-06-17 22:38:31 -0400

    I set up the startup config to get the jwt private key

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 
-
+require('./startup/routes')(app);
+require('./startup/db')();
+require('./startup/config');
 
 const port = process.env.PORT || 8888;
 const server = app.listen(port, function () {
---
+++ startup/config.js
@@ -0,0 +1,10 @@
+const config = require('config');
+
+module.exports = function () {
+    //have to set this environment variable first
+    //if it doesn't exist then exit the program
+    //export vidly_jwtPrivateKey=mySecureKey
+    if(!config.get('jwtPrivateKey')){
+        throw new Error('FATAL ERROR: jwt private key is not defined');
+    }
+}
