007e2c18fa29b3479583cfb5a3d666685957bf99
Alexander Watkins <afw821@gmail.com>
2019-06-18 22:52:43 -0400

    I finished the customer api routes

--- routes/customers.js
+++ routes/customers.js
@@ -11,7 +11,7 @@ router.post('/', async function (req, res) {
     //validate the req.body object
     const result = validate(req.body);
     //if invalid return a 404 error to the client
-    if(result.error) {
+    if (result.error) {
         res.status(404).send(result.error.details[0].message);
         return;
     }
@@ -29,10 +29,47 @@ router.post('/', async function (req, res) {
 router.get('/:id', async function (req, res) {
     const customer = await Customer.findById(req.params.id);
     //if course doesn't exist return 404
-    if(!customer) {
+    if (!customer) {
         res.status(404).send('The requested id does not exist');
         return;
     }
 
     res.send(customer);
 });
+router.put('/:id', async function (req, res) {
+    //validate user input
+    const result = validate(req.body);
+    //if invalid return 400 bad request
+    if (result.error) {
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+    const customer = await Customer.findByIdAndUpdate(req.params.id, {
+        name: req.body.name,
+        phone: req.body.phone,
+        isGold: req.body.isGold
+    }, {
+            new: true // new true returns the new updated genre
+        });
+    //if the customer doesn't exist return 404
+    if (!customer) {
+        res.status(404).send("The requested genre doesn't exist");
+        return;
+    }
+    //send response to the client
+    res.send(customer);
+});
+
+router.delete('/:id', async function (req, res) {
+    const customer = await Customer.findByIdAndRemove( req.params.id);
+
+    //if customer doesn't exist send 404 error to the client
+    if (!customer) {
+        res.send(404).send("the requested customer doesn't exist");
+        return;
+    }
+    //return removed customer
+    res.send(customer);
+});
+
+module.exports = router;
