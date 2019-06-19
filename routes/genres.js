759c7ad1acf79396ef2e93d1eb0ca3491135aa9d
Alexander Watkins <afw821@gmail.com>
2019-06-18 23:21:44 -0400

    I added the genres api routes

---
+++ routes/genres.js
@@ -0,0 +1,71 @@
+const { Genre, validate } = require('../models/genre');
+const express = require('express');
+const router = express.Router();
+
+//get all genres
+router.get('/', async function (req, res) {
+    try{
+        const genres = await Genre.find();
+        res.send(genres);
+    }catch(exception) {
+        console.log(exception);
+    }
+});
+//get genre by id
+router.get('/:id', async function (req, res) {
+    const genre = await Genre.findById(req.params.id);
+    //if course doesn't exist return 404
+    if (!genre) {
+        res.status(404).send("The requested id doesn't exist");
+        return;
+    }
+    //return genre to the client
+    res.send(genre);
+});
+//post a genre
+router.post('/', async function (req, res) {
+    //validate the req.body object
+    const result = validate(req.body);
+    //if invalid return a 404 error to the client
+    if (result.error) {
+        res.status(404).send(result.error.details[0].message);
+        return;
+    }
+    //define the object that you want to post(push to array)
+    let genre = new Genre({ name: req.body.name })
+    //else push the course object to the course array
+    genre = await genre.save();
+    //send the response back to the client
+    res.send(genre);
+});
+//put(replace) route
+router.put('/:id', async function (req, res) {
+    //validate user input
+    const result = validate(req.body);
+    //if invalid return 400 bad request
+    if (result.error) {
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+    const genre = await Genre.findByIdAndUpdate(req.params.id, { name: req.body.name }, {
+        new: true // new true returns the new updated genre
+    });
+    //if the requested course doesn't exist return 404
+    if (!genre) {
+        res.status(404).send("The requested genre doesn't exist");
+        return;
+    }
+    //send response to the client
+    res.send(genre);
+});
+router.delete('/:id', async function (req, res) {
+    const genre = await Genre.findByIdAndRemove( req.params.id);
+    //if genre doesn't exist send 404 error to the client
+    if (!genre) {
+        res.send(404).send("the requested genre doesn't exist");
+        return;
+    }
+    //return removed genre from the array
+    res.send(genre);
+});
+module.exports = router;
--- startup/routes.js
+++ startup/routes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path');
-
+const customers = require('../routes/customers');
 module.exports = function (app) {
+    app.use('/api/customers', customers);
     app.use(express.json());
     app.use(express.static(path.join(__dirname, 'public')));
 }
