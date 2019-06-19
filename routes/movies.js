f161ec09b6db1c4420db65195ce745a7bd91e19b
Alexander Watkins <afw821@gmail.com>
2019-06-19 01:01:56 -0400

    I added a put and delete route to the movies api route

--- routes/movies.js
+++ routes/movies.js
@@ -1,5 +1,5 @@
-const {Movie, validate} = require('../models/movie');
-const {Genre} = require('../models/genre');
+const { Movie, validate } = require('../models/movie');
+const { Genre } = require('../models/genre');
 const express = require('express');
 const router = express.Router();
 
@@ -13,14 +13,14 @@ router.post('/', async function (req, res) {
     //validate the req.body from the client
     const result = validate(req.body);
     //if error res to client 400 bad request
-    if(result.error){
+    if (result.error) {
         res.status(400).send(result.error.details[0].message);
         return;
     }
     //find genre by genreId
     const genre = await Genre.findById(req.body.genreId);
-    if(!genre) return res.status(400).send('Invalid genre');
-
+    if (!genre) return res.status(400).send('Invalid genre');
+    //create a new movie object with the genre _id and name properties
     let movie = new Movie({
         title: req.body.title,
         genre: {
@@ -31,6 +31,44 @@ router.post('/', async function (req, res) {
         dailyRentalRate: req.body.dailyRentalRate
     });
     movie = await movie.save();
-  
+
     res.send(movie);
 });
+//update route
+router.put('/:id', async function (req, res) {
+    //validating the request body because we are sending a new movie object to update a current one
+    const result = validate(req.body);
+    if (result.error) {
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+    //finding the genre by id
+    const genre = await Genre.findById(req.body.genreId);
+    //if genre doesn't exist return bad req to client
+    if (!genre) return res.status(400).send('Invalid Genre');
+    //define a new movie object to update after find by id
+    const movie = await Movie.findByIdAndUpdate(req.params.id,
+        {
+            title: req.body.title,
+            genre: {
+                _id: genre._id,
+                name: genre.name
+            },
+            numberInStock: req.body.numberInStock,
+            dailyRentalRate: req.body.dailyRentalRate
+        }, { new: true });
+    //if movie not found then return 404 to client
+    if (!movie) return res.status(404).send('The movie with the given ID was not found.');
+    //else send movie to the client 
+    res.send(movie);
+});
+//delete route
+router.delete('/:id', async function (req, res) {
+    const movie = await Movie.findByIdAndRemove(req.params.id);
+
+    if (!movie) return res.status(404).send('The movie with the given ID was not found.');
+
+    res.send(movie);
+});
+
+module.exports = router;
