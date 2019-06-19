1e8fe998831336bb187e46c0970e94b6f8bf38f9
Alexander Watkins <afw821@gmail.com>
2019-06-18 21:16:38 -0400

    I finished to movie model and added a Joi validate movie function

--- models/movie.js
+++ models/movie.js
@@ -16,5 +16,30 @@ const movieSchema = new mongoose.Schema({
     genre: {
         type: genreSchema,
         required: true
+    },
+    numberInStock: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 255
+    },
+    dailyRentalRate: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 255
     }
-})
+});
+const Movie = mongoose.model('Movies', movieSchema);
+
+function validateMovie(movie) {
+    const schema = {
+        title: Joi.string().min(5).max(50).required(),
+        genreId: Joi.string().required(),
+        numberInStock: Joi.number().min(0).required(),
+        dailyRentalRate: Joi.number().min(0).required()
+    };
+    return Joi.validate(movie, schema);
+}
+module.exports.Movie = Movie;
+module.exports.validate = validateMovie;
