17a476e37f5f17f8edc283d5386db93510a3d3db
Alexander Watkins <afw821@gmail.com>
2019-09-15 21:32:17 -0400

    added new mongo db connection string

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const app = express();
 const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json()); 
+app.use(express.json());
 require('./startup/routes')(app);
 require('./routes/html')(app);
 require('./startup/db')();
 require('./startup/config')();
-
+console.log('mongo db ui', process.env);
 const PORT = process.env.PORT || 8888;
 const environment = `Environment: ${process.env.NODE_ENV}`;
 console.log(environment);
--- startup/db.js
+++ startup/db.js
@@ -2,16 +2,17 @@ const mongoose = require('mongoose');
 const config = require('config');
 module.exports = function () {
 
-    if (process.env.MONGODB_URI) {
-        mongoose.connect(process.env.MONGODB_URI);
-    } else {
-        //const db = config.get('db')
-        mongoose.connect("mongodb://localhost/vidly_node", { useNewUrlParser: true })
-            .then(function () {
-                console.log(`Connected to MongoDB!!!!!`)
-            }).catch(function (ex) {
-                console.log('Error connecting to MongoDB');
-            });
-    }
+    // if (process.env.MONGODB_URI) {
+    //     mongoose.connect(process.env.MONGODB_URI);
+    // } else {
+    //const db = config.get('db')
+    //mongoose.connect("mongodb://localhost/vidly_node", { useNewUrlParser: true })
+    mongoose.connect("mongodb://heroku_ts7csbwm:6rjkom7bics89mcfqtdbi3h1gj@ds353007.mlab.com:53007/heroku_ts7csbwm", { useNewUrlParser: true })
+        .then(function () {
+            console.log(`Connected to MongoDB!!!!!`)
+        }).catch(function (ex) {
+            console.log('Error connecting to MongoDB');
+        });
+    //}
 
 }
