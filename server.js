a8f845eba0fff26dfac522da3f94ad080243dee6
Alexander Watkins <afw821@gmail.com>
2019-09-15 22:01:31 -0400

    I added the db connection logic to startup js to debug the deployment issue

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 require('./startup/routes')(app);
 require('./routes/html')(app);
-require('./startup/db')();
+// require('./startup/db')();
 require('./startup/config')();
 console.log('mongo db ui', process.env);
+
+
+if (process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI);
+} else {
+    //const db = config.get('db')
+    mongoose.connect("mongodb://localhost/vidly_node", { useNewUrlParser: true })
+        .then(function () {
+            console.log(`Connected to MongoDB!!!!!`)
+        }).catch(function (ex) {
+            console.log('Error connecting to MongoDB');
+        });
+}
+
+
+
 const PORT = process.env.PORT || 8888;
 const environment = `Environment: ${process.env.NODE_ENV}`;
 console.log(environment);
