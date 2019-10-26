e367c7ca9423068f75e6daa4db19be8712fbb563
Alexander Watkins <afw821@gmail.com>
2019-10-25 21:21:37 -0400

    I removed the auth middleware function on post movies so I could post movies to MLAB from postman

--- routes/movies.js
+++ routes/movies.js
@@ -1,104 +1,108 @@
-const { Movie, validate } = require('../models/movie');
-const { Genre } = require('../models/genre');
-const express = require('express');
+const { Movie, validate } = require("../models/movie");
+const { Genre } = require("../models/genre");
+const express = require("express");
 const router = express.Router();
-const auth = require('../middleware/auth');
+const auth = require("../middleware/auth");
 //get all movies
-router.get('/', auth, async function (req, res) {
-    try {
-        const movies = await Movie.find();
-        res.send(movies);
-    } catch (ex) {
-        console.log('ex', ex);
-    }
-
+router.get("/", auth, async function(req, res) {
+  try {
+    const movies = await Movie.find();
+    res.send(movies);
+  } catch (ex) {
+    console.log("ex", ex);
+  }
 });
 //post a new movie to DB
-router.post('/', auth, async function (req, res) {
-    //validate the req.body from the client
+router.post("/", async function(req, res) {
+  //validate the req.body from the client
+  const result = validate(req.body);
+  //if error res to client 400 bad request
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+  //find genre by genreId
+  const genre = await Genre.findById(req.body.genreId);
+  if (!genre) return res.status(400).send("Invalid genre");
+  //create a new movie object with the genre _id and name properties
+  let movie = new Movie({
+    title: req.body.title,
+    genre: {
+      _id: genre._id,
+      name: genre.name
+    },
+    numberInStock: req.body.numberInStock,
+    dailyRentalRate: req.body.dailyRentalRate
+  });
+  movie = await movie.save();
+
+  res.send(movie);
+});
+//update route
+router.put("/:id", auth, async function(req, res) {
+  try {
+    //validating the request body because we are sending a new movie object to update a current one
+
     const result = validate(req.body);
-    //if error res to client 400 bad request
     if (result.error) {
-        res.status(400).send(result.error.details[0].message);
-        return;
+      res.status(400).send(result.error.details[0].message);
+      return;
     }
-    //find genre by genreId
+    //finding the genre by id
     const genre = await Genre.findById(req.body.genreId);
-    if (!genre) return res.status(400).send('Invalid genre');
-    //create a new movie object with the genre _id and name properties
-    let movie = new Movie({
+    //if genre doesn't exist return bad req to client
+    if (!genre) return res.status(400).send("Invalid Genre");
+    //define a new movie object to update after find by id
+    const movie = await Movie.findByIdAndUpdate(
+      req.params.id,
+      {
         title: req.body.title,
         genre: {
-            _id: genre._id,
-            name: genre.name
+          _id: genre._id,
+          name: genre.name
         },
         numberInStock: req.body.numberInStock,
         dailyRentalRate: req.body.dailyRentalRate
-    });
-    movie = await movie.save();
-
+      },
+      { new: true }
+    );
+    //if movie not found then return 404 to client
+    if (!movie)
+      return res.status(404).send("The movie with the given ID was not found.");
+    //else send movie to the client
     res.send(movie);
-});
-//update route
-router.put('/:id', auth, async function (req, res) {
-    try {
-        //validating the request body because we are sending a new movie object to update a current one
-
-        const result = validate(req.body);
-        if (result.error) {
-            res.status(400).send(result.error.details[0].message);
-            return;
-        }
-        //finding the genre by id
-        const genre = await Genre.findById(req.body.genreId);
-        //if genre doesn't exist return bad req to client
-        if (!genre) return res.status(400).send('Invalid Genre');
-        //define a new movie object to update after find by id
-        const movie = await Movie.findByIdAndUpdate(req.params.id,
-            {
-                title: req.body.title,
-                genre: {
-                    _id: genre._id,
-                    name: genre.name
-                },
-                numberInStock: req.body.numberInStock,
-                dailyRentalRate: req.body.dailyRentalRate
-            }, { new: true });
-        //if movie not found then return 404 to client
-        if (!movie) return res.status(404).send('The movie with the given ID was not found.');
-        //else send movie to the client 
-        res.send(movie);
-    } catch (ex) {
-        console.log('FATAL ERROR:::', ex);
-    }
-
+  } catch (ex) {
+    console.log("FATAL ERROR:::", ex);
+  }
 });
 //get movie by id
-router.get('/:id', async function (req, res) {
-    try {
-        const movie = await Movie.findById(req.params.id);
+router.get("/:id", async function(req, res) {
+  try {
+    const movie = await Movie.findById(req.params.id);
 
-        if (!movie) return res.status(404).send('The movie with thr requested id was not found!!');
+    if (!movie)
+      return res
+        .status(404)
+        .send("The movie with thr requested id was not found!!");
 
-        res.send(movie);
-    } catch (ex) {
-        res.send(ex);
-        console.log('FATAL ERROR::', ex);
-    }
-
+    res.send(movie);
+  } catch (ex) {
+    res.send(ex);
+    console.log("FATAL ERROR::", ex);
+  }
 });
 //delete route
-router.delete('/:id', async function (req, res) {
-    try {
-        const movie = await Movie.findByIdAndRemove(req.params.id);
-        console.log(movie);
-        if (!movie) return res.status(404).send('The movie with the given ID was not found.');
-        console.log('Movie exists');
-        res.send(movie);
-    } catch (ex) {
-        console.log('FATAL ERROR::', ex);
-    }
-
+router.delete("/:id", async function(req, res) {
+  try {
+    const movie = await Movie.findByIdAndRemove(req.params.id);
+    console.log(movie);
+    if (!movie)
+      return res.status(404).send("The movie with the given ID was not found.");
+    console.log("Movie exists");
+    res.send(movie);
+  } catch (ex) {
+    console.log("FATAL ERROR::", ex);
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,43 +1,41 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const mongoose = require('mongoose');
+const path = require("path");
+const mongoose = require("mongoose");
 
+const customers = require("./routes/customers");
+const genres = require("./routes/genres");
+const movies = require("./routes/movies");
+const rentals = require("./routes/rentals");
+const users = require("./routes/users");
+const auth = require("./routes/auth");
 
-const customers = require('./routes/customers');
-const genres = require('./routes/genres');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const users = require('./routes/users');
-const auth = require('./routes/auth');
-
-
-
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // require('./startup/routes')(app);
-require('./routes/html')(app);
+require("./routes/html")(app);
 // require('./startup/db')();
-require('./startup/config')();
-mongoose.connect(process.env.MONGODB_URI);
+require("./startup/config")();
+//mongoose.connect(process.env.MONGODB_URI);
 
-app.use('/api/auth', auth);
-app.use('/api/users', users);
-app.use('/api/rentals', rentals);
-app.use('/api/genres', genres);
-app.use('/api/movies', movies);
-app.use('/api/customers', customers);
+app.use("/api/auth", auth);
+app.use("/api/users", users);
+app.use("/api/rentals", rentals);
+app.use("/api/genres", genres);
+app.use("/api/movies", movies);
+app.use("/api/customers", customers);
 
 const TEST_URI = "mongodb://localhost/vidly_node";
 
-// mongoose.connect(MONGODB_URI || TEST_URI, { useNewUrlParser: true }).then(function () {
-//     console.log('Connected to MongoDB!!');
-// })
-//     .catch(function () {
-//         console.log('Error connecting to MongoDB');
-//     });
-
+mongoose
+  .connect(process.env.MONGODB_URI || TEST_URI, { useNewUrlParser: true })
+  .then(function() {
+    console.log("Connected to MongoDB!!");
+  })
+  .catch(function() {
+    console.log("Error connecting to MongoDB");
+  });
 
 // if (process.env.MONGODB_URI) {
 //     mongoose.connect(process.env.MONGODB_URI);
@@ -51,13 +49,11 @@ const TEST_URI = "mongodb://localhost/vidly_node";
 //         });
 // }
 
-
-
 const PORT = process.env.PORT || 8888;
 // const environment = `Environment: ${process.env.NODE_ENV}`;
 // console.log(environment);
-const server = app.listen(PORT, function () {
-    console.log(`App listening on PORT: ${PORT}`);
+const server = app.listen(PORT, function() {
+  console.log(`App listening on PORT: ${PORT}`);
 });
 
 module.exports = server;
