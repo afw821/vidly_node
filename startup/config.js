bee129c5f5b5512a3b89f16fcd4f71dfbbe345b5
Alex Watkins <afw821@gmail.com>
2019-10-27 17:48:40 -0400

    testing commit from new machine

--- startup/config.js
+++ startup/config.js
@@ -3,7 +3,7 @@ const config = require('config');
 module.exports = function () {
     
     //have to set this environment variable first
-    //if it doesn't exist then exit the program
+    //if it doesn't exist then exit the progra
     //export vidly_node_jwtPrivateKey=mySecureKey
     if(!config.get('jwtPrivateKey')){
         throw new Error('FATAL ERROR: jwt private key is not defined');
