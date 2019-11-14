0d0d69a448625ef1702aa5c9ce13db19d2b24592
Alex Watkins <afw821@gmail.com>
2019-11-13 20:31:55 -0500

    I finsihed adding express async hander for all of the api routes and removed all try / catch blocks

--- routes/users.js
+++ routes/users.js
@@ -2,35 +2,19 @@ const { User, validate } = require('../models/user');
 const express = require('express');
 const bcrypt = require('bcrypt');
 const router = express.Router();
-//authorization--user has permission to access resource or not
 const auth = require('../middleware/auth');
-const path = require('path');
+const admin = require('../middleware/admin');
+const ash = require('express-async-handler');
 
-//get information about the current user
-router.get('/me', auth, async function (req, res) {
-    try {
-        //more secure than passing id as a parameter
-        //exclude password property
+router.get('/me', auth, ash(async function (req, res) {
         const user = await User.findById(req.user._id).select('-password');
         res.send(user);
-    } catch (ex) {
-        console.log('error', ex);
-    }
-
-});
-//register a new user
-router.post('/', async function (req, res) {
-    console.log('post user req body', req.body);
-    //validate the user input
+}));
+//register a user
+router.post('/', ash(async function (req, res) {
     const result = validate(req.body);
-    if (result.error) {
-
-        res.status(404).send(result.error.details[0].message);
-        return;
-    }
-    //lookup user by email and if the exist return 400
+    if (result.error) return res.status(404).send(result.error.details[0].message);   
     let user = await User.findOne({ email: req.body.email });
-    //if user already exists in the database by email then return
     if (user) return res.status(400).send('User already registered');
     //else create the new user
     user = new User({
@@ -42,25 +26,19 @@ router.post('/', async function (req, res) {
     //salt and has passord using bcrypt
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(user.password, salt);
-    //savit the new user
+    //save the new user
     await user.save();
-
     //generate jwt with function in user.js
     const token = user.generateAuthToken();
     //after generating the token set it to the response header
     //exclude the password from being sent
     //res.header('x-auth-token', token);
     res.send(user);
-});
+}));
 
-router.get('/', async function (req, res) {
-    try {
+router.get('/', [ auth, admin ], ash(async function (req, res) {
         const users = await User.find();
         res.send(users);
-    } catch (ex) {
-        console.log('ex:', ex);
-    }
-
-});
+}));
 
 module.exports = router;
