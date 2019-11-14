e63ed58fc24d857a1525c8051f81e74357285468
Alex Watkins <afw821@gmail.com>
2019-11-13 19:42:59 -0500

    I completed theexpress asyn handler for all of the genre routes

--- routes/genres.js
+++ routes/genres.js
@@ -24,44 +24,20 @@ router.post('/', ash(async function (req, res) {
         genre = await genre.save();
         res.send(genre);
 }));
-//put(replace) route
-router.put('/:id', auth, async function (req, res) {
-    try {
-        //validate user input
+
+router.put('/:id', auth, ash(async function (req, res) {
         const result = validate(req.body);
-        //if invalid return 400 bad request
-        if (result.error) {
-            res.status(400).send(result.error.details[0].message);
-            return;
-        }
+        if (result.error) return  res.status(400).send(result.error.details[0].message);        
         const genre = await Genre.findByIdAndUpdate(req.params.id, { name: req.body.name }, {
             new: true // new true returns the new updated genre
         });
-        //if the requested course doesn't exist return 404
-        if (!genre) {
-            res.status(404).send("The requested genre doesn't exist");
-            return;
-        }
-        //send response to the client
+        if (!genre) return  res.status(404).send("The requested genre doesn't exist");
         res.send(genre);
-    } catch (ex) {
-        console.log('FATAL ERROR', ex);
-    }
+}));
 
-});
-router.delete('/:id', [auth, admin], async function (req, res) {
-    try {
+router.delete('/:id', ash(async function (req, res) {
         const genre = await Genre.findByIdAndRemove(req.params.id);
-        //if genre doesn't exist send 404 error to the client
-        if (!genre) {
-            res.send(404).send("the requested genre doesn't exist");
-            return;
-        }
-        //return removed genre from the array
+        if (!genre) return  res.status(404).send("the requested genre doesn't exist");        
         res.send(genre);
-    } catch (ex) {
-        console.log('FATAL ERROR::', ex);
-    }
-
-});
+}));
 module.exports = router;
