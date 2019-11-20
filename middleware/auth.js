e4dff1620f68637e5a02bda79c771f1da645120f
Alexander Watkins <afw821@gmail.com>
2019-11-19 23:27:09 -0500

    I updated the config to get the jwt private key in the auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -12,7 +12,7 @@ function auth(req, res, next) {
 
 
         // //id there is a token we need to verify it's a valid token
-        const decoded = jwt.verify(token,'jwtPrivateKey');
+        const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
         // //ABOVE WILL BE DECODED PAYLOAD
         //Decoded user object from Sign method
         req.user = decoded;
