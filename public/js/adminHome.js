e0f155b1219c8bd24f81a9fe2830b81bee246579
Alex Watkins <afw821@gmail.com>
2019-11-23 23:18:56 -0500

    I removed the module because it was not working

--- public/js/adminHome.js
+++ public/js/adminHome.js
@@ -1,5 +1,5 @@
 (async function () {
-    import reindexArray from './helperFunctions/reindexArray';
+
     //Redirect if user isn't logged in
     const token = sessionStorage.getItem('x-auth-token');
     if (!token) {
@@ -106,39 +106,39 @@
         window.location.href = '/admin';
     });
     
-    // function reindexArray(args) {
+    function reindexArray(args) {
        
-    //     const reindexArray = [];
+        const reindexArray = [];
 
-    //     $(args).find('option').each(function (index, element) {
+        $(args).find('option').each(function (index, element) {
 
-    //         if ($(this).val()) {
-    //             const value = $(element).val();
-    //             const name = $(element).text();
-    //             let reindexObject = { name: name, value: value }
-    //             reindexArray.push(reindexObject);
-    //         }
-    //     });
+            if ($(this).val()) {
+                const value = $(element).val();
+                const name = $(element).text();
+                let reindexObject = { name: name, value: value }
+                reindexArray.push(reindexObject);
+            }
+        });
 
-    //     $(args).find('option').first().siblings().remove();
-    //     // sort by name
-    //     reindexArray.sort(function (a, b) {
-    //         var nameA = a.name.toUpperCase();
-    //         var nameB = b.name.toUpperCase();
-    //         if (nameA > nameB) {
-    //             return -1;
-    //         }
-    //         if (nameA < nameB) {
-    //             return 1;
-    //         }
-    //         return 0;
-    //     });
-    //     reindexArray.forEach(function (element, index, arr) {
-    //         const id = element.value;
-    //         const name = element.name;
-    //         const sortedElement = `<option class="option-genre" value="${id}">${name}</option>`;
+        $(args).find('option').first().siblings().remove();
+        // sort by name
+        reindexArray.sort(function (a, b) {
+            var nameA = a.name.toUpperCase();
+            var nameB = b.name.toUpperCase();
+            if (nameA > nameB) {
+                return -1;
+            }
+            if (nameA < nameB) {
+                return 1;
+            }
+            return 0;
+        });
+        reindexArray.forEach(function (element, index, arr) {
+            const id = element.value;
+            const name = element.name;
+            const sortedElement = `<option class="option-genre" value="${id}">${name}</option>`;
 
-    //         $(args).find('option').first().after(sortedElement);
-    //     });
-    // }
+            $(args).find('option').first().after(sortedElement);
+        });
+    }
 })();
