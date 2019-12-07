2382e16d93be12c438e332fe51c18ccd4eba9871
Alex Watkins <afw821@gmail.com>
2019-12-07 18:10:06 -0500

    I got the reviews to display on the homepage

--- public/js/login.js
+++ public/js/login.js
@@ -177,6 +177,87 @@ $(document).ready(function () {
       });
 
       console.log('reviews', reviews);
+      for(let i = 0; i < reviews.length; i++) {
+        console.log(reviews[i]);
+
+        const comment = reviews[i].comment;
+        const date = reviews[i].date;
+        const subject = reviews[i].subject;
+        const stars = reviews[i].stars;
+        const userId = reviews[i].user._id;
+        const userEmail = reviews[i].user.email;
+        const userName = reviews[i].user.name;
+        
+        const reviewWrapper = $('<div>', {
+          class: "card text-center review-wrapper-div mb-4",
+          id: userId
+        });
+        const reviewHeader = $('<div>', {
+          class: "review-header card-header",
+          text: `By: ${userName}  ${date}`,
+          appendTo: reviewWrapper
+        });
+        const reviewBody = $('<div>', {
+          class: "card-body review-body",
+          appendTo: reviewWrapper
+        });
+        const reviewSubject = $('<h5>', {
+          class: "card-title review-subject",
+          text: subject,
+          appendTo: reviewBody
+        });
+        const reviewComment = $('<p>', {
+          class: "card-text review-comment",
+          text: comment,
+          appendTo: reviewBody
+        });
+        const reviewStarsDiv = $('<div>', {
+          class: "card-footer text-muted review-stars-div",
+          appendTo: reviewWrapper
+        });
+        const oneStar = $('<span>', {
+          class: function () {
+            if(stars >= 1) return 'fa fa-star one-star checked';
+
+            return 'fa fa-star one-star';
+          },
+          appendTo: reviewStarsDiv
+        });
+        const twoStar = $('<span>', {
+          class: function () {
+            if(stars >= 2) return 'fa fa-star two-star checked';
+
+            return 'fa fa-star two-star';
+          },
+          appendTo: reviewStarsDiv
+        });
+        const threeStar = $('<span>', {
+          class: function () {
+            if(stars >= 3) return 'fa fa-star three-star checked';
+
+            return 'fa fa-star three-star';
+          },
+          appendTo: reviewStarsDiv
+        });
+        const fourStar = $('<span>', {
+          class: function () {
+            if(stars >= 4) return 'fa fa-star four-star checked';
+
+            return 'fa fa-star four-star';
+          },
+          appendTo: reviewStarsDiv
+        });
+        const fiveStar = $('<span>', {
+          class: function () {
+            if(stars == 5) return 'fa fa-star five-star checked';
+
+            return 'fa fa-star five-star';
+          },
+          appendTo: reviewStarsDiv
+        });
+
+        $('.customer-review-div').append(reviewWrapper);
+      }
     }catch(ex) {
       alert('There was an error!!');
     }
